469c1469f9de94fd0c85bd91ced64f18c1a721bc
root <root@webrtc-gw4.broadsoftlabs.com>
2016-06-09 14:21:16 +0000

    Added "Diversion" header for ability to specify transfer origin

--- src/Grammar/dist/Grammar.js
+++ src/Grammar/dist/Grammar.js
@@ -189,6 +189,7 @@ ExSIP.Grammar = (function(){
         "From": parse_From,
         "from_param": parse_from_param,
         "tag_param": parse_tag_param,
+        "Diversion": parse_Diversion,
         "Max_Forwards": parse_Max_Forwards,
         "Min_Expires": parse_Min_Expires,
         "Name_Addr_Header": parse_Name_Addr_Header,
@@ -9357,6 +9358,73 @@ ExSIP.Grammar = (function(){
         }
         if (result0 !== null) {
           result0 = (function(offset, tag) {data.tag = tag; })(pos0, result0[2]);
+        }
+        if (result0 === null) {
+          pos = pos0;
+        }
+        return result0;
+      }
+      
+      function parse_Diversion() {
+        var result0, result1, result2, result3;
+        var pos0, pos1, pos2;
+        
+        pos0 = pos;
+        pos1 = pos;
+        result0 = parse_SIP_URI_noparams();
+        if (result0 === null) {
+          result0 = parse_name_addr();
+        }
+        if (result0 !== null) {
+          result1 = [];
+          pos2 = pos;
+          result2 = parse_SEMI();
+          if (result2 !== null) {
+            result3 = parse_generic_param();
+            if (result3 !== null) {
+              result2 = [result2, result3];
+            } else {
+              result2 = null;
+              pos = pos2;
+            }
+          } else {
+            result2 = null;
+            pos = pos2;
+          }
+          while (result2 !== null) {
+            result1.push(result2);
+            pos2 = pos;
+            result2 = parse_SEMI();
+            if (result2 !== null) {
+              result3 = parse_generic_param();
+              if (result3 !== null) {
+                result2 = [result2, result3];
+              } else {
+                result2 = null;
+                pos = pos2;
+              }
+            } else {
+              result2 = null;
+              pos = pos2;
+            }
+          }
+          if (result1 !== null) {
+            result0 = [result0, result1];
+          } else {
+            result0 = null;
+            pos = pos1;
+          }
+        } else {
+          result0 = null;
+          pos = pos1;
+        }
+        if (result0 !== null) {
+          result0 = (function(offset) {
+                        try {
+                          data = new ExSIP.NameAddrHeader(data.uri, data.display_name, data.params);
+                        } catch(e) {
+                          data = -1;
+                        }})(pos0);
         }
         if (result0 === null) {
           pos = pos0;
